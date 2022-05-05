3f6e63a2fc13f7591cfe82d53880f1949387e653
ZZXACER\57159 <571599138@qq.com>
2022-05-05 15:30:13 +0800

    数据库支持和获取所有讨论消息

--- 论坛（前后端）/后端（node.js）/online_forum/index.js
+++ 论坛（前后端）/后端（node.js）/online_forum/index.js
@@ -3,6 +3,8 @@ const Koa = require('koa');  //koa框架
 const Router = require("koa-router") //koa路由
 // 引入自定义的子路由
 const user_route=require('./my_routes/user');
+const talks_route=require('./my_routes/talks');
+
 
 // 创建对象
 const app = new Koa();
@@ -16,6 +18,9 @@ router.get("/", (ctx) => {
 
 // 使用子路由
 router.use('/user',user_route) //访问 /user 路径下的页面都会使用这个子路由
+router.use('/talks',talks_route) //访问 /talks 路径下的页面都会使用这个子路由
+
+
 //使用路由
 app.use(router.routes())
 
---
+++ 论坛（前后端）/后端（node.js）/online_forum/my_routes/talks.js
@@ -0,0 +1,47 @@
+// 论坛中讨论（帖子）相关的API
+
+const Router = require("koa-router") //路由
+const router = new Router()
+
+var sqlite3 = require('sqlite3').verbose() //数据库
+
+// 数据库的路径
+var db_path = './database/online_forum.db'
+
+router.get('/', async (ctx) => {
+    ctx.body = 'talks 首页'
+})
+
+// 返回所有的讨论列表
+router.get('/showall', async (ctx) => {
+    var dt = await showall();//等待数据库返回数据
+    ctx.body = {
+        data: dt
+    }
+}
+)
+
+// 获取所有的讨论列表
+function showall() {
+    return new Promise(function (resolve) {
+        // 打开sqlites数据库
+        var db;
+        db = new sqlite3.Database(db_path, function (err) {
+            if (err) throw err;
+        })
+        // all查询所有数据
+        db.all(`select * from talks`, function (err, row) {
+            if (err) throw err
+            else {
+                // console.log('all查询结果 ', row)
+                resolve(row)
+            }
+        })
+    })
+
+}
+
+
+
+// 导出
+module.exports = router.routes()
